3eed78bd38458e6187a5d76ccc08abb5a83d6ab3
Az21 <39803478+Az-21@users.noreply.github.com>
2021-01-11 21:20:02 +0530

    init

---
+++ README.md
@@ -0,0 +1,12 @@
+# Custom Word List Combiner for skribbl.io
+
+## Function
+* Reads all `.txt` files in the working directory
+* Creates a combined output at `output.txt` (reserved; won't read as input)
+
+## Format
+Each `.txt` input file must be a comma separated list. Example:
+
+```
+Alpha, beta 123, gamma delta, Zeta
+```
--- customCombine.py
+++ customCombine.py
@@ -1,17 +1,21 @@
 import glob, os
 
+# Initialize list
 customWords = []
 
-os.chdir("C:\\Github\\skribbl")
+# Read all .txt files
+dir_path = os.path.dirname(os.path.realpath(__file__))
+os.chdir(dir_path)
 for file in glob.glob("*.txt"):
-    with open(file, 'r') as f:
-        f_contents = f.read()
-        customWordTemp = f_contents.split(',')
-        customWords.extend(customWordTemp)
+    if(file != 'output.txt'):
+        with open(file, 'r') as f:
+            f_contents = f.read()
+            customWordTemp = f_contents.split(',')
+            customWords.extend(customWordTemp)
 
 
-#print(customWords)
-
-strCustomWords = ', '.join(map(str, customWords))
+# Write output.txt with all responses combined
+filter(None, customWords)                               # remove blank elements
+strCustomWords = ', '.join(map(str, customWords))       # list to str
 with open('output.txt', 'w') as f:
     f.write(strCustomWords)
